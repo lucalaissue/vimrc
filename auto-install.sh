1b8cd69b418ee0a69d132d72e98033f89d566ea8
Luca Laissue <l.github.com@laissue.ch>
2020-09-18 11:01:46 +0200

    Get files from gitlab

---
+++ auto-install.sh
@@ -0,0 +1,14 @@
+# Install vim-plug for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install vim-plug for nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install settings for vim
+curl -fLo ~/.vimrc --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/.vimrc
+curl -fLo ~/.vim/coc-settings.json --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/coc-settings.json
+
+# Install settings for nvim
+curl -fLo ~/.config/nvim/init.vim --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/.vimrc
+curl -fLo ~/.config/nvim/coc-settings.json --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/coc-settings.json
+
