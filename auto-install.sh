29e33c0a3d47cbee3b83691f4c6b4be978ec8d73
Luca Laissue <l.github.com@laissue.ch>
2020-09-18 11:09:08 +0200

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -5,10 +5,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install settings for vim
-curl -fLo ~/.vimrc --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/.vimrc
-curl -fLo ~/.vim/coc-settings.json --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/coc-settings.json
+curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/lucalaissue/vimrc/master/.vimrc
+curl -fLo ~/.vim/coc-settings.json --create-dirs https://raw.githubusercontent.com/lucalaissue/vimrc/master/coc-settings.json
 
 # Install settings for nvim
-curl -fLo ~/.config/nvim/init.vim --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/.vimrc
-curl -fLo ~/.config/nvim/coc-settings.json --create-dirs https://gitlab.com/lucalaissue/vimrc/-/raw/master/coc-settings.json
-
+curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/lucalaissue/vimrc/master/.vimrc
+curl -fLo ~/.config/nvim/coc-settings.json --create-dirs https://raw.githubusercontent.com/lucalaissue/vimrc/master/coc-settings.json
